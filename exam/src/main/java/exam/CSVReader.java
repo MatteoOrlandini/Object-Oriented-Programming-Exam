55e820522c237bb5d0236056ef96314ac801d23d
MatteoOrlandini <48382335+MatteoOrlandini@users.noreply.github.com>
2019-06-05 14:33:10 +0200

    added ";" character control

--- exam/src/main/java/exam/CSVReader.java
+++ exam/src/main/java/exam/CSVReader.java
@@ -5,17 +5,19 @@ import java.util.Vector;
 
 public class CSVReader {
 	private static String fileName;
-	
+
 	public CSVReader(String fileName) {
 		fileName = this.fileName;
 	}
-	
+
 	public static String getFileName() {
 		return fileName;
 	}
+
 	public static void setFileName(String fileName) {
 		CSVReader.fileName = fileName;
 	}
+
 	public static void reader() {
 
 		String csvFile = fileName;
@@ -30,41 +32,39 @@ public class CSVReader {
 			while ((line = br.readLine()) != null) {
 
 				String[] test = line.split(cvsSplitBy);
-				//for (String s : test) {
+				// for (String s : test) {
 				///////////////////////////////////////
-				/*try{
-		            float i = Float.valueOf(str); //Float.parseFloat(str); 
-		         
-		            System.out.println("Value parsed :"+i);
-		        }catch(NumberFormatException ex){
-		            System.err.println("Ilegal input");
-		            // Discard input or request new input ...
-		            // clean up if necessary
-		        } 
-		        */
+				/*
+				 * try{ float i = Float.valueOf(str); //Float.parseFloat(str);
+				 * 
+				 * System.out.println("Value parsed :"+i); }catch(NumberFormatException ex){
+				 * System.err.println("Ilegal input"); // Discard input or request new input ...
+				 * // clean up if necessary }
+				 */
 				////////////////////////////////////////
-				if (test[14].contains(",")) {
-					test[14] = test[14].replace(',', '.');
-					System.out.println(test[14]);
+				for (int i = 0; i < test.length; i++) {
+
+					if (test[i].contains(",")) {
+						test[i] = test[i].replace(',', '.');
+						System.out.println(test[i]);
+					}
+					if (test[i].contains(";")) {
+						test[i] = test[i].replace(';', '.');
+						System.out.println(test[i]);
+					}
+
 				}
-			
-			if (test[15].contains(",")) {
-				test[15] = test[15].replace(',', '.');
-				System.out.println(test[15]);
+
+				pharmacies.add(new Pharmacy(Double.parseDouble(test[0]), test[1], test[2], Double.parseDouble(test[3]),
+						Double.parseDouble(test[4]), Double.parseDouble(test[5]), test[6], Double.parseDouble(test[7]),
+						test[8], test[9], Double.parseDouble(test[10]), test[11], test[12], test[13],  Double.parseDouble(test[14]),  Double.parseDouble(test[15]),
+						Double.parseDouble(test[16])));
+			//for (Pharmacy item : pharmacies) {
+			//	System.out.println(pharmacies.toString());
+
+			//	}
 			}
-			
-				//}
-					pharmacies.add(new Pharmacy(Double.parseDouble(test[0]), test[1], test[2],
-							Double.parseDouble(test[3]), Double.parseDouble(test[4]), Double.parseDouble(test[5]),
-							test[6], Double.parseDouble(test[7]), test[8], test[9], Double.parseDouble(test[10]),
-							test[11], test[12], test[13], 14, 15, Double.parseDouble(test[16])));
-				}	
-				for(Pharmacy item : pharmacies) {		
-						System.out.println(pharmacies.toString());
-					
-				}
-	}
-	 catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
