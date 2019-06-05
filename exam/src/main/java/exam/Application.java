71b4c465ce960a47ae5d66a5e4dd50851bb83cda
MatteoOrlandini <48382335+MatteoOrlandini@users.noreply.github.com>
2019-06-05 09:47:03 +0200

    added JSONparseranddownload class

--- exam/src/main/java/exam/Application.java
+++ exam/src/main/java/exam/Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		String url = "https://www.dati.gov.it/api/3/action/package_show?id=3c68b286-09fd-447a-b8e3-1b8430f70969";
+		JSONparseranddownload.parser(url);
 		SpringApplication.run(Application.class, args);
 	}
 
---
+++ exam/src/main/java/exam/JSONparseranddownload.java
@@ -0,0 +1,72 @@
+package exam;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
+public class JSONparseranddownload {
+
+	//public static void main(String[] args) {
+	public static void parser(String url){
+		//if(args.length == 1)
+			//url = args[0]; //Url by args ;-)
+		try {
+			
+			URLConnection openConnection = new URL(url).openConnection();
+			openConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+			InputStream in = openConnection.getInputStream();
+			
+			 String data = "";
+			 String line = "";
+			 try {
+			   InputStreamReader inR = new InputStreamReader( in );
+			   BufferedReader buf = new BufferedReader( inR );
+			  
+			   while ( ( line = buf.readLine() ) != null ) {
+				   data+= line;
+				   System.out.println( line );
+			   }
+			 } finally {
+			   in.close();
+			 }
+			JSONObject obj = (JSONObject) JSONValue.parseWithException(data); 
+			JSONObject objI = (JSONObject) (obj.get("result"));
+			JSONArray objA = (JSONArray) (objI.get("resources"));
+			
+			for(Object o: objA){
+			    if ( o instanceof JSONObject ) {
+			        JSONObject o1 = (JSONObject)o; 
+			        String format = (String)o1.get("format");
+			        String urlD = (String)o1.get("url");
+			        System.out.println(format + " | " + urlD);
+			        if(format.equals("csv")) {
+			        	download(urlD, "t1.csv");
+			        }
+			    }
+			}
+			System.out.println( "OK" );
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	//}
+	
+	public static void download(String url, String fileName) throws Exception {
+	    try (InputStream in = URI.create(url).toURL().openStream()) {
+	        Files.copy(in, Paths.get(fileName));
+	    }
+	}
+}
