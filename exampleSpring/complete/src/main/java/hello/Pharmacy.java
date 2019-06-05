5295f41088392d0a9932d645ea4a09dd3572c2c4
MatteoOrlandini <48382335+MatteoOrlandini@users.noreply.github.com>
2019-06-05 07:43:43 +0200

    created pharmacy class

---
+++ exampleSpring/complete/src/main/java/hello/Pharmacy.java
@@ -0,0 +1,188 @@
+package hello;
+
+import java.util.List;
+
+public class Pharmacy {
+	private long id;
+	private String name;
+	private String address;
+	private long VATNumber;
+	private long CAP;
+	private long ISTATCityCode;
+	private String city;
+	private long ISTATProvinceCode;
+	private String provinceAbbreviation;
+	private String provinceName;
+	private long regionCode;
+	private String regionName;
+	private String beginningValidity;
+	private String endValidity;
+	private long latitude;
+	private long longitude;
+	private long localize;
+	
+	public Pharmacy(long id, String name, String address, long vATNumber, long cAP, long iSTATCityCode, String city,
+			long iSTATProvinceCode, String provinceAbbreviation, String provinceName, long regionCode,
+			String regionName, String beginningValidity, String endValidity, long latitude, long longitude,
+			long localize) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		VATNumber = vATNumber;
+		CAP = cAP;
+		ISTATCityCode = iSTATCityCode;
+		this.city = city;
+		ISTATProvinceCode = iSTATProvinceCode;
+		this.provinceAbbreviation = provinceAbbreviation;
+		this.provinceName = provinceName;
+		this.regionCode = regionCode;
+		this.regionName = regionName;
+		this.beginningValidity = beginningValidity;
+		this.endValidity = endValidity;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.localize = localize;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public long getVATNumber() {
+		return VATNumber;
+	}
+
+	public void setVATNumber(long vATNumber) {
+		VATNumber = vATNumber;
+	}
+
+	public long getCAP() {
+		return CAP;
+	}
+
+	public void setCAP(long cAP) {
+		CAP = cAP;
+	}
+
+	public long getISTATCityCode() {
+		return ISTATCityCode;
+	}
+
+	public void setISTATCityCode(long iSTATCityCode) {
+		ISTATCityCode = iSTATCityCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public long getISTATProvinceCode() {
+		return ISTATProvinceCode;
+	}
+
+	public void setISTATProvinceCode(long iSTATProvinceCode) {
+		ISTATProvinceCode = iSTATProvinceCode;
+	}
+
+	public String getProvinceAbbreviation() {
+		return provinceAbbreviation;
+	}
+
+	public void setProvinceAbbreviation(String provinceAbbreviation) {
+		this.provinceAbbreviation = provinceAbbreviation;
+	}
+
+	public String getProvinceName() {
+		return provinceName;
+	}
+
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+
+	public long getRegionCode() {
+		return regionCode;
+	}
+
+	public void setRegionCode(long regionCode) {
+		this.regionCode = regionCode;
+	}
+
+	public String getRegionName() {
+		return regionName;
+	}
+
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+
+	public String getBeginningValidity() {
+		return beginningValidity;
+	}
+
+	public void setBeginningValidity(String beginningValidity) {
+		this.beginningValidity = beginningValidity;
+	}
+
+	public String getEndValidity() {
+		return endValidity;
+	}
+
+	public void setEndValidity(String endValidity) {
+		this.endValidity = endValidity;
+	}
+
+	public long getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(long latitude) {
+		this.latitude = latitude;
+	}
+
+	public long getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(long longitude) {
+		this.longitude = longitude;
+	}
+
+	public long getLocalize() {
+		return localize;
+	}
+
+	public void setLocalize(long localize) {
+		this.localize = localize;
+	}
+	
+	
+
+	
+	
+}
---
+++ exampleSpring/complete/src/main/java/hello/StudentService.java
@@ -0,0 +1,109 @@
+package hello;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import ch.qos.logback.core.property.ResourceExistsPropertyDefiner;
+
+
+@Component
+public class StudentService {
+
+	private static List<Student> students = new ArrayList<>();
+
+	static {
+		//Initialize Data
+		Course course1 = new Course("Course1", "Spring", "10 Steps", Arrays
+				.asList("Learn Maven", "Import Project", "First Example",
+						"Second Example"));
+		Course course2 = new Course("Course2", "Spring MVC", "10 Examples",
+				Arrays.asList("Learn Maven", "Import Project", "First Example",
+						"Second Example"));
+		Course course3 = new Course("Course3", "Spring Boot", "6K Students",
+				Arrays.asList("Learn Maven", "Learn Spring",
+						"Learn Spring MVC", "First Example", "Second Example"));
+		Course course4 = new Course("Course4", "Maven",
+				"Most popular maven course on internet!", Arrays.asList(
+						"Pom.xml", "Build Life Cycle", "Parent POM",
+						"Importing into Eclipse"));
+
+		Student ranga = new Student("Student1", "Ranga Karanam",
+				"Hiker, Programmer and Architect", new ArrayList<>(Arrays
+						.asList(course1, course2, course3, course4)));
+
+		Student satish = new Student("Student2", "Satish T",
+				"Hiker, Programmer and Architect", new ArrayList<>(Arrays
+						.asList(course1, course2, course3, course4)));
+
+		students.add(ranga);
+		students.add(satish);
+	}
+
+	public List<Student> retrieveAllStudents() {
+		return students;
+	}
+
+	public Student retrieveStudent(String studentId) {
+		for (Student student : students) {
+			if (student.getId().equals(studentId)) {
+				return student;
+			}
+		}
+		return null;
+	}
+
+	public List<Course> retrieveCourses(String studentId) {
+		Student student = retrieveStudent(studentId);
+		
+		if(studentId.equalsIgnoreCase("Student1")){
+			throw new RuntimeException("Something went wrong");
+		}
+
+		if (student == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+
+		return student.getCourses();
+	}
+
+	public Course retrieveCourse(String studentId, String courseId) {
+		Student student = retrieveStudent(studentId);
+
+		if (student == null) {
+			return null;
+		}
+
+		for (Course course : student.getCourses()) {
+			if (course.getId().equals(courseId)) {
+				return course;
+			}
+		}
+
+		return null;
+	}
+
+	private SecureRandom random = new SecureRandom();
+
+	public Course addCourse(String studentId, Course course) {
+		Student student = retrieveStudent(studentId);
+
+		if (student == null) {
+			return null;
+		}
+
+		String randomId = new BigInteger(130, random).toString(32);
+		course.setId(randomId);
+
+		student.getCourses().add(course);
+
+		return course;
+	}
+}
