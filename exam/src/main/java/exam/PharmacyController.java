002de342646222ccd2c9d3041f90103dbaffbbcb
MatteoOrlandini <48382335+MatteoOrlandini@users.noreply.github.com>
2019-06-06 23:52:12 +0200

    Update PharmacyController.java

--- exam/src/main/java/exam/PharmacyController.java
+++ exam/src/main/java/exam/PharmacyController.java
@@ -2,9 +2,17 @@ package exam;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 import java.util.Vector;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.BasicJsonParser;
+import org.springframework.boot.json.JsonParser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,4 +40,41 @@ public class PharmacyController {
 	public String si() {
 		return "si";
 	}
+	/*
+	//i parametri possono essere passati mediante una POST (nel body della richiesta)
+	@PostMapping(value = "/filter")
+	public String filter(@RequestBody String param) {
+		try {
+			JsonParser jsonParser = new BasicJsonParser();
+			Map<String, Object> jsonMap = null;
+			jsonMap = jsonParser.parseMap(param);
+		} catch (Exception e) {
+			throw new RuntimeException("Filed", e);
+		}
+
+		JSONObject obj = null;
+		try {
+			obj = (JSONObject) JSONValue.parseWithException(param);
+			*/
+			/*
+			 * esempio JSON
+			   {
+					"metadata": "id",
+					"operator": "in", 
+					"value": ["2000","5000"]
+				}
+			 */
+	/*
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		JSONObject objO = (JSONObject) (obj.get("metadata"));
+		System.out.println(objO);
+		JSONObject objO2 = (JSONObject) (obj.get("operator"));
+		System.out.println(objO2);
+		JSONArray objA = (JSONArray) (obj.get("value"));
+		System.out.println(objA);
+		return new String("filter. Body: " + param);
+	}*/
 }
