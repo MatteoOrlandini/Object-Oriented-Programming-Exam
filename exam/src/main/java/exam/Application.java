3f9374318a3915f6577cbe19c540d3e89292518d
Jacopo14 <pagliucajacopo@gmail.com>
2019-06-06 10:56:29 +0200

    added UML

--- exam/src/main/java/exam/Application.java
+++ exam/src/main/java/exam/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		JSONparseranddownload.setFileName("dataset.csv");
-		JSONparseranddownload.setUrl( "https://www.dati.gov.it/api/3/action/package_show?id=3c68b286-09fd-447a-b8e3-1b8430f70969");
+		JSONparseranddownload.setUrl("https://www.dati.gov.it/api/3/action/package_show?id=3c68b286-09fd-447a-b8e3-1b8430f70969");
 		JSONparseranddownload.parser();
 		String fileName = JSONparseranddownload.getFileName();
 		CSVReader.setFileName(fileName);
