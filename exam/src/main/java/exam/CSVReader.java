2418e96b0204ddcd6cada2c4ac02f76455b7654e
MatteoOrlandini <48382335+MatteoOrlandini@users.noreply.github.com>
2019-06-06 12:09:00 +0200

    CSV Reader is now reading correct

--- exam/src/main/java/exam/CSVReader.java
+++ exam/src/main/java/exam/CSVReader.java
@@ -41,9 +41,20 @@ public class CSVReader {
 			br.readLine();
 			while ((line = br.readLine()) != null) {
 				if (line.contains("\"Via Cappuccini ;163\"")) {
-					System.out.println(line);
+					// System.out.println(line);
 					line = line.replace("\"Via Cappuccini ;163\"", "\"Via Cappuccini ,163\"");
-					System.out.println(line);
+					// System.out.println(line);
+				}
+
+				if (line.contains("\"via vespucci;26\"")) {
+					// System.out.println(line);
+					line = line.replace("\"via vespucci;26\"", "\"via vespucci,26\"");
+					// System.out.println(line);
+				}
+				if (line.contains("\'9,258444")) {
+					// System.out.println(line);
+					line = line.replace("\'9,258444", "9.258444");
+					// System.out.println(line);
 				}
 
 				String[] test = line.split(cvsSplitBy);
@@ -52,29 +63,32 @@ public class CSVReader {
 					test[14] = test[14].replace(',', '.');
 					// System.out.println(test[14]);
 				}
+				if (test[14].contains("-")) {
+					test[14] = test[14].replace('-', '0');
+					// System.out.println(test[15]);
+				}
 				if (test[15].contains(",")) {
 					test[15] = test[15].replace(',', '.');
 					// System.out.println(test[15]);
 				}
-				try {
-					pharmacies.add(new Pharmacy(Double.parseDouble(test[0]), test[1], test[2],
-							Double.parseDouble(test[3]), Double.parseDouble(test[4]), Double.parseDouble(test[5]),
-							test[6], Double.parseDouble(test[7]), test[8], test[9], Double.parseDouble(test[10]),
-							test[11], test[12], test[13], Double.parseDouble(test[14]), Double.parseDouble(test[15]),
-							Double.parseDouble(test[16])));
-				} catch (NumberFormatException ex) {
-					// Thrown to indicate that the application has attempted to convert a string to
-					// one of the numeric types, but that the string does not have the appropriate
-					// format.
-					System.err.println("Ilegal input");
-
+				if (test[15].contains("-")) {
+					test[15] = test[15].replace('-', '0');
+					// System.out.println(test[15]);
 				}
 
-				for (Pharmacy item : pharmacies) {
-					System.out.println(item.toString() + "\n");
+				pharmacies.add(new Pharmacy(Double.parseDouble(test[0]), test[1], test[2], Double.parseDouble(test[3]),
+						Double.parseDouble(test[4]), Double.parseDouble(test[5]), test[6], Double.parseDouble(test[7]),
+						test[8], test[9], Double.parseDouble(test[10]), test[11], test[12], test[13],
+						Double.parseDouble(test[14]), Double.parseDouble(test[15]), Double.parseDouble(test[16])));
 
-				}
 			}
+
+		} catch (NumberFormatException ex) {
+			// Thrown to indicate that the application has attempted to convert a string to
+			// one of the numeric types, but that the string does not have the appropriate
+			// format.
+			System.err.println("Illegal input");
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -83,12 +97,17 @@ public class CSVReader {
 			if (br != null) {
 				try {
 					br.close();
+					System.out.println("\nCLOSED\n");
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 		}
 
+		for (Pharmacy item : pharmacies) {
+			System.out.println(item.toString() + "\n");
+
+		}
 	}
 
 }
